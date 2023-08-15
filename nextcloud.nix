ed2af78869c3cb7adb99ae83388ccb63b289a5b7
francescoDeSimone <francesco.desimone@canonical.com>
2023-08-15 13:18:58 +0200

    add redis and some apps to nextcloud

--- nextcloud.nix
+++ nextcloud.nix
@@ -44,6 +44,29 @@
         package = pkgs.nextcloud27;
         hostName = "nextcloud.desimedia.duckdns.org";
         extraAppsEnable = true;
+        configureRedis = true;
+        extraApps = {
+          deck = pkgs.fetchNextcloudApp rec {
+            url = "https://github.com/nextcloud-releases/deck/releases/download/v1.10.0/deck-v1.10.0.tar.gz";
+            sha256 = "sha256-8HavzNtdyzc4TDyxsuG8V4uew3VfeQon0FLbx51HRe4=";
+          };
+          memories = pkgs.fetchNextcloudApp rec {
+            url = "https://github.com/pulsejet/memories/releases/download/v5.2.1/memories.tar.gz";
+            sha256 = "sha256-NGss+UBvwtDI73CLMc0gdVLCTp0YnNSeWzv/U6yD4mw=";
+          };
+          richdocuments = pkgs.fetchNextcloudApp rec {
+            url = "https://github.com/nextcloud-releases/richdocuments/releases/download/v8.1.1/richdocuments-v8.1.1.tar.gz";
+            sha256 = "sha256-SkKEviqtq9823T7i2R6Fe8lLJFPxJgfxLMu6kTe8XRw=";
+          };
+          mail = pkgs.fetchNextcloudApp rec {
+            url = "https://github.com/nextcloud-releases/mail/releases/download/v1.14.1/mail-v1.14.1.tar.gz";
+            sha256 = "sha256-sQUsYC3cco6fj9pF2l1NrCEhA3KJoOvJRhXvBlVpNqo=";
+          };
+          contacts = pkgs.fetchNextcloudApp rec {
+            url = "https://github.com/nextcloud-releases/contacts/releases/download/v4.2.2/contacts-v4.2.2.tar.gz";
+            sha256 = "sha256-eTc51pkg3OdHJB7X4/hD39Ce+9vKzw1nlJ7BhPOzdy0=";
+          };
+        };
         config = {
           dbtype = "pgsql";
           dbuser = "nextcloud";
